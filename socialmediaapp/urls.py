feafffabce87da05154726ab2a97677c1326ac23
A78009866 <salemdopamine@gmail.com>
2025-03-05 22:02:12 +0100

    Fix STATIC_ROOT for collectstatic

--- socialmediaapp/urls.py
+++ socialmediaapp/urls.py
@@ -3,12 +3,13 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import login_view, logout_view, signup_view, edit_profile, profile_view
+from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns = [
     path('signup', views.signup_view, name='signup'),
-    path('login/', login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout', views.logout_view, name='logout'),
     path('home', views.home_view, name='home'),
     path('post/create/', views.post_create_view, name='post_create'),
